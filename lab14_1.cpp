06a6b68572413bca2ed6bfcb9652c32670afdb92
Turborocket004 <Turborocket004@gmail.com>
2023-01-13 17:16:39 +0700

    lab14_1done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double st = 1;
+    double sum = 0;
+    double max = 0,min = 99;
+    for(int i = 0;i < N;i++){
+        sum += A[i];
+        B[1] += pow(A[i],2);
+        B[3] += 1/A[i];
+        st *= A[i];
+        
+        }
+        B[0] = sum/N; 
+        B[1] = sqrt((B[1]/N)-pow(B[0],2));
+        B[2] = pow(st,(1.0/N));
+        B[3] = N/B[3];
+    for(int i = 0;i<N;i++){
+            if(A[i] > max){
+                max = A[i];
+                }
+        B[4] = max;        
+            if(A[i] < min){
+                min = A[i];
+                }
+        B[5]= min;
+        }
+}        
